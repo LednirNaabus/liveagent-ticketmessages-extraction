7261c6b16995bfa0714b266b21beb8214f9239db
Lednir <valsubaan@yahoo.com>
2025-04-29 15:18:07 +0800

    Add '--per_page' as argument

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import argparse
 from core.liveagent_client import tickets, get_ticket_messages, ping
+from config import config
 
-if __name__ == "__main__":
+def parse_arguments():
     parser = argparse.ArgumentParser(description="Fetch tickets and messages using LiveAgent API.")
     parser.add_argument(
-        "--mp",
+        "--max_pages",
         type=int,
-        default=5,
-        help="Max pages to fetch (default: 5, max page is '_perPage' in LiveAgent API)"
+        default=1,
+        help="Max pages to fetch (default: 1, max page is '_page' in LiveAgent API)"
     )
-    args = parser.parse_args()
+    parser.add_argument(
+        "--per_page",
+        type=int,
+        default=10,
+        help="Number of records to fetch per page (default: 10, per page is '_perPage' in LiveAgent API)"
+    )
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_arguments()
     success, res = ping()
     if success:
-        all_tickets = tickets(max_pages=args.mp)
-        df = get_ticket_messages(all_tickets, max_pages=args.mp)
+        config.ticket_payload["_page"] = args.max_pages
+        config.ticket_payload["_perPage"] = args.per_page
+        config.messages_payload["_perPage"] = args.per_page
+
+        all_tickets = tickets(max_pages=args.max_pages)
+        df = get_ticket_messages(all_tickets, max_pages=args.max_pages)
         print(df)
         df.to_csv("out.csv", index=False)
     else:
