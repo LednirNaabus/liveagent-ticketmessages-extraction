e4d600deec71e6878f63d5cf392158ff89d37dfb
Val Subaan <valsubaan@yahoo.com>
2025-05-23 16:36:36 +0800

    Refactor core/extract_tickets_date.py - make it a callable function instead of argparse

--- core/extract_tickets_date.py
+++ core/extract_tickets_date.py
@@ -1,31 +1,9 @@
 import json
-import asyncio
 import aiohttp
-import argparse
 import datetime
+import pandas as pd
 from config import config
 from core.liveagent_client import async_agents, async_tickets_filtered, fetch_all_messages
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description="Fetch tickets and messages using LiveAgent API (one day).")
-    parser.add_argument(
-        "--date", "-d",
-        type=str,
-        help="The date you want to extract (YYYY-MM-DD)"
-    )
-    parser.add_argument(
-        "--max_pages", "-mp",
-        type=int,
-        default=1,
-        help="Max pages to fetch (default: 1, max page is '_page' in LiveAgent API)"
-    )
-    parser.add_argument(
-        "--per_page", "-pp",
-        type=int,
-        default=10,
-        help="Number of records to fetch per page (default: 10, per page is '_perPage' in LiveAgent API)"
-    )
-    return parser.parse_args()
 
 def set_filter(start: str, end: str):
     return json.dumps([
@@ -33,29 +11,37 @@ def set_filter(start: str, end: str):
         ["date_created", "D<=", f"{end} 23:59:59"]
     ])
 
-async def get_tickets_day():
-    args = parse_arguments()
-    config.ticket_payload["_page"] = args.max_pages
-    config.ticket_payload["_perPage"] = args.per_page
-    config.messages_payload["_perPage"] = args.per_page
-    today_date = args.date
+def drop_cols(df: pd.DataFrame) -> pd.DataFrame:
+    try:
+        cols_to_drop = ['message_id', 'type', 'agentid']
+        existing = [col for col in cols_to_drop if col in df.columns]
 
-    # today_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        if existing:
+            df.drop(columns=existing, axis=1, inplace=True)
+        else:
+            pass
+    except Exception as e:
+        print(df.columns)
+        print(f"Exception: {e}")
+    return df
+
+async def process_tickets_day():
+    config.ticket_payload["_page"] = 100
+    config.ticket_payload["_perPage"] = 100
+    config.messages_payload["_perPage"] = 100
+
+    today_date = datetime.datetime.today().strftime("%Y-%m-%d")
     async with aiohttp.ClientSession() as session:
         agents = await async_agents(session)
         agents_lookup = dict(zip(agents["id"], agents["name"]))
 
-        # ticket_payload = config.ticket_payload.copy()
         config.ticket_payload["_filters"] = set_filter(today_date, today_date)
         print(config.ticket_payload["_filters"])
         tickets = await async_tickets_filtered(session, config.ticket_payload, 1)
-        print(tickets.keys())
 
         messages_df = await fetch_all_messages(tickets, agents_lookup, 1)
         print(messages_df.keys())
         # columns to remove:
         # message_id, type, agent_id
-        messages_df.drop(columns=['message_id', 'type', 'agentid'], inplace=True)
-        messages_df.to_csv("testing.csv", index=False)
-
-asyncio.run(get_tickets_day())
+        messages_df = drop_cols(messages_df)
+        messages_df.to_csv("testing-test.csv", index=False)
